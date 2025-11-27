47b31ec3cff405e70a0b244dc39e397ddddd9f9c
LekoArts <lekoarts@gmail.com>
2025-11-27 17:29:45 +0100

    update deps

--- src/mastra/workflows/index.ts
+++ src/mastra/workflows/index.ts
@@ -1,9 +1,8 @@
-import { openai } from '@ai-sdk/openai';
 import { Agent } from '@mastra/core/agent';
 import { createStep, createWorkflow } from '@mastra/core/workflows';
 import { z } from 'zod';
 
-const llm = openai('gpt-4o');
+const llm = "openai/gpt-4o";
 
 const agent = new Agent({
   name: 'Weather Agent',
